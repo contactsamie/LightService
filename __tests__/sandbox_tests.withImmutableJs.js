5d5070a5d1522251e040811438a8c047aefb13fc
Sam <contactsamie@gmail.com>
2016-02-13 14:37:45 -0500

    set up test with Immutable JS included

---
+++ __tests__/sandbox_tests.withImmutableJs.js
@@ -0,0 +1,12 @@
+﻿jest.dontMock('../lib/js/immutable.min');
+jest.dontMock('../__tests__/sandbox_tests');
+var Immutable = require('../lib/js/immutable.min');
+if (!Immutable) {
+    throw "Immutable JS not found"
+} else {
+    console.info("Testing With Immutable JS");
+}
+
+console.info("using immutable JS");
+require('../__tests__/sandbox_tests');
+
