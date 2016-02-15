67c0efc67b4caaa34a689f7d00954b4a008b4fb7
Sam <contactsamie@gmail.com>
2016-02-15 15:38:00 -0500

    update

--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,84 @@
-﻿
-light.handle("validator", function (definition) {
-  
+﻿var log = "";
+light(function () {
+    this.system.startRecording();
 });
+light.handle("addOne", function (definition) {
+});
+//0
 light.service("doubler", function (arg) {
+    log += ",0";
     return arg * 2;
 });
 
-
+//1
 light.receive("message", function (message) {
-    var data1 = this.stor.get("arg");
+    log += ",1";
+    var data1 = this.store.get("arg");
     console.log(data1);
     this.store.set("arg", 100)
-    var data2 = this.stor.get("arg");
+    var data2 = this.store.get("arg");
     console.log(data2);
 
     console.log(this.arg);
 
-    this.service().doubler(message);
+    // return this.service().doubler(message);
 });
-
+//2
 light.receive("message", function (message) {
-    var data1 = this.stor.get("arg");
+    log += ",2";
+    var data1 = this.store.get("arg");
     console.log(data1);
-    this.store.set("arg", 100)
-    var data2 = this.stor.get("arg");
+    this.store.set("arg", 200)
+    var data2 = this.store.get("arg");
     console.log(data2);
 
     console.log(this.arg);
 
-    this.service().doubler(message);
+    return this.service().doubler(message);
 });
 
+//3
+light(function () {
+    var data1 = this.store.get("arg");
+    console.log(data1);
+    this.store.set("arg", 100)
+    var data2 = this.store.get("arg");
+    console.log(data2);
+
+    console.log(this.arg);
+
+    var data3 = light.send("message", 123);
+
+    console.log(data3);
+});
+//4
+light(function () {
+    var data1 = this.store.get("arg");
+    console.log(data1);
+    this.store.set("arg", 200)
+    var data2 = this.store.get("arg");
+    console.log(data2);
+
+    console.log(this.arg);
+
+    var data3 = light.send("message", 456);
+
+    console.log(data3);
+});
 
 light(function () {
-    var data1 = this.stor.get("arg");
-    console.log(data1);
-    this.store.set("arg", 100)
-    var data2 = this.stor.get("arg");
-    console.log(data2);
-
-    console.log(this.arg);
-
-    light.send("message", 123);
+    this.system.stopRecording();
 });
+
 light(function () {
-    var data1 = this.stor.get("arg");
-    console.log(data1);
-    this.store.set("arg", 100)
-    var data2 = this.stor.get("arg");
-    console.log(data2);
+    log += "|";
 
-    console.log(this.arg);
+    this.service().timemachine_last();
+    this.service().timemachine_previous();
+    this.service().timemachine_previous();
+    this.service().timemachine_previous();
+    this.service().timemachine_previous();
+    this.service().timemachine_previous();
 
-    light.send("message", 123);
+    console.log(log);
 });
